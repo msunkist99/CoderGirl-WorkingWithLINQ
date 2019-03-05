dad8df4b0c13565933e81c188932418d7b2a25af
msunkist99 <msunkist99@gmail.com>
2019-03-05 13:57:35 -0600

    Add project files.

---
+++ WorkingWithLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
